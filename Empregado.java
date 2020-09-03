75e20c14bd47650d503a10f2e345de26937eb3f0
William Ribeiro Medeiros <william.rm0410@gmail.com>
2020-09-03 20:37:02 -0300

    Arquivo modificado

--- Empregado.java
+++ Empregado.java
@@ -1,6 +1,8 @@
 public class Empregado{
     private String nome;
     private int idade; 
-
-    
+    private int tipo;
+    private double salario;
+    private double comissao;
+    private double bonus;
 }
